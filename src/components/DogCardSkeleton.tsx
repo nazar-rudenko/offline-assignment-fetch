60dccda703704564424661fdf39cfec0b58d082b
Nazar Rudenko <6103832+nazar-rudenko@users.noreply.github.com>
2025-02-14 17:03:48 -0600

    fix: trying to improve LCP

--- src/components/DogCardSkeleton.tsx
+++ src/components/DogCardSkeleton.tsx
@@ -1,7 +1,11 @@
 const DogCardSkeleton = () => {
   return (
     <div className="card bg-base-100 shadow-md rounded-lg overflow-hidden h-[380px] animate-pulse">
-      <div className="skeleton h-48 w-full"></div>
+      <img
+        src="/src/assets/dog_paw.png"
+        alt="Dog Paw Placeholder"
+        className="h-48 w-full object-contain filter opacity-20 aspect-square"
+      />
 
       <div className="p-4">
         <div className="skeleton h-6 w-3/4 mb-2"></div>
